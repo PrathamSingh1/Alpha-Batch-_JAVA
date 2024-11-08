52643513341e46a80e4cf682bd298aabd930b25d
Pratham Singh <prathamjii36@gmail.com>
2024-11-08 23:48:47 +0530

    reverseArray

--- Arrays/reverseArray.java
+++ Arrays/reverseArray.java
@@ -16,5 +16,13 @@ public class reverseArray {
     }
     public static void main(String[] args) {
         int numbers[] = {2, 4, 6, 8, 10};
+
+        reverse(numbers);
+
+        //print array
+        for(int i=0; i<numbers.length; i++) {
+            System.out.print(numbers[i]+" ");
+        }
+        System.out.println();
     }
 }
